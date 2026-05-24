3c18367e115c51c8ed77a2daf771dabebdb189ac
phucle1011 <phuc628780@gmail.com>
2026-05-24 18:05:58 +0700

    fix đường dẫn

--- config/url.js
+++ config/url.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:5000/';
-export const FRONTEND_URL = 'http://localhost:3000/';
+export const BACKEND_URL = 'https://web-tong-hop-be.onrender.com';
+export const FRONTEND_URL = 'https://web-tong-hop-fe.vercel.app/';
